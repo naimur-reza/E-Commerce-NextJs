f1609fb56cda17751a0ff3703018d048986d64ae
Naimur Reza <naimur.rezaa@gmail.com>
2023-10-19 20:23:54 +0600

    image type fix

--- app/utils/uploadImage.ts
+++ app/utils/uploadImage.ts
@@ -1,4 +1,4 @@
-export const uploadImage = async (image) => {
+export const uploadImage = async (image: string | Blob) => {
   const formData = new FormData();
   formData.append("image", image);
   const url = `https://api.imgbb.com/1/upload?key=f55c73443021ed1be6ed658d21091e38`;
